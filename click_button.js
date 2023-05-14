c254cf77667b873264427dac1e3f17db5a9f051a
jessicazanella <48051642+jessicazanella@users.noreply.github.com>
2023-05-14 10:57:09 +0200

    Add files via upload

---
+++ click_button.js
@@ -0,0 +1,6 @@
+
+const buyButton = document.querySelector('.buy-button');
+      buyButton.addEventListener('click', function(event) { 
+        event.preventDefault(); 
+        alert('Thank you for your purchase!');
+      });
