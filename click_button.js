af94ffb1617aff10b47001dfee6139b9403c2255
jessicazanella <48051642+jessicazanella@users.noreply.github.com>
2023-05-14 11:51:01 +0200

    Update click_button.js

--- click_button.js
+++ click_button.js
@@ -1,6 +1,5 @@
-
 const buyButton = document.querySelector('.buy-button');
-      buyButton.addEventListener('click', function(event) { 
-        event.preventDefault(); 
-        alert('Thank you for your purchase!');
-      });
+buyButton.addEventListener('click', function(event) { 
+  event.preventDefault(); 
+  alert('Thank you for your purchase!');
+});
