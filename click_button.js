b39ea2d4ea511d3bc34c9cb59fd2c617f9ada000
jessicazanella <48051642+jessicazanella@users.noreply.github.com>
2023-05-14 11:14:46 +0200

    Update click_button.js

--- click_button.js
+++ click_button.js
@@ -1,5 +1,5 @@
 
-const buyButton = document.querySelector('.buy-button');
+const buyButton = document.querySelector('click');
       buyButton.addEventListener('click', function(event) { 
         event.preventDefault(); 
         alert('Thank you for your purchase!');
